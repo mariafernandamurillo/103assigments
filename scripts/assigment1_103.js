263a44f3054ff2d46d3c1227f1214b492d016279
Invito Digital <fernanda.murillo@uabc.edu.mx>
2023-01-28 12:04:28 -0800

    Assigment 1 course 103

--- scripts/assigment1_103.js
+++ scripts/assigment1_103.js
@@ -17,33 +17,34 @@ let subscriptions = ["Channel 1", "...", "Channel n"];
 let channelDescription = "The channel description"; 
 let communityPosts = "A community post";
 let likedComment = true;
-let profilePicture = "images/profile_picture.png";
+let profilePicture = "folder/profile_picture.png";
+let coverPicture = "folder/cover_picture.png";
+let interfaceLaunguage = ["Spanish", "English", "...", "Language n" ];
+let interfazMode = ["Device theme", "Dark", "Light "];
+let notificationPreferences = "True or False";
+let paymentMethod = ["Credit card", "Debit card"];
 
 document.write(
     `
-    <h3>As user</h3>
     <p class="text"> 1. Subscribed =   ${isSubscribed}</p>
     <p class="text"> 2. Reproduction List =   ${reproductionList}</p>
     <p class="text"> 3. Video Volume = ${videoVolume [5]}</p>
     <p class="text"> 4. Liked video =   ${likedVideo}</p>
     <p class="text"> 5. A comment =   ${videoComment}</p>
-    <p class="text"> 6. = ${}</p>
-    <p class="text"> 7. = ${}</p>
-    <p class="text"> 8. = ${}</p>
-    <p class="text"> 9. = ${}</p>
-    <p class="text"> 10. = ${}</p>
-    <br>
-    
-    <h3>As content creator</h3>
-    <p class="text"> 1. Video title = ${videoTitle}  
-    <p class="text"> 2. Video description = ${videoDescription}</p>
-    <p class="text"> 3. Name of the channel = ${channelName}</p>
-    <p class="text"> 4. Uploeaded videos list = ${uploadedVideosList}</p>
-    <p class="text"> 5. Instagram link in the cover = ${instagramLink}</p>
-    <p class="text"> 6. Subscriptions = ${subscriptions}</p>
-    <p class="text"> 7.  Channel description = ${channelDescription}</p>
-    <p class="text"> 8. Community posts = ${communityPosts}</p>
-    <p class="text"> 9. Like in a comment = ${likedComment}</p>
-    <p class="text"> 10. The profile picture = ${profilePicture}</p>
+    <p class="text"> 6. Video title = ${videoTitle}  
+    <p class="text"> 7. Video description = ${videoDescription}</p>
+    <p class="text"> 8. Name of the channel = ${channelName}</p>
+    <p class="text"> 9. Uploeaded videos list = ${uploadedVideosList}</p>
+    <p class="text"> 10. Instagram link in the cover = ${instagramLink}</p>
+    <p class="text"> 11. Subscriptions = ${subscriptions}</p>
+    <p class="text"> 12.  Channel description = ${channelDescription}</p>
+    <p class="text"> 13. Community posts = ${communityPosts}</p>
+    <p class="text"> 14. Like in a comment = ${likedComment}</p>
+    <p class="text"> 15. The profile picture = ${profilePicture}</p>
+    <p class="text"> 16. The cover picture = ${coverPicture}</p>
+    <p class="text"> 17. The interface language = ${interfaceLaunguage}</p>
+    <p class="text"> 18. Interface mode  = ${interfazMode}</p>
+    <p class="text"> 19. Notifications preferences  = ${notificationPreferences}</p>
+    <p class="text"> 20. Payment method  = ${paymentMethod}</p>
     </p>
     `);
